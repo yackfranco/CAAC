5dbea98d0ea62850bc9978bb2bd22e2f5dabaf0a
yackfranco <jesusandres548@gmail.com>
2017-04-04 08:33:59 -0500

    Estructuracion del CAAC
    1. se reestructuro el modulo de registro
    
    - registrar Apiarios
    -registrar Camaras
    - Registrar Colmenas
    - registrar Inventario
    - registrar Marcos
    - registrar Usuarios
    - Menu registrar
    - Se realizo los controller de cada uno de los templates
    - Se resuelve conflicto en el archivo app.config.js

--- webApp/app/app.config.js
+++ webApp/app/app.config.js
@@ -10,17 +10,33 @@ angular.module('CAAC').
                             controller: 'controlApiariosController',
                             templateUrl: 'app/template/ControlApiarios.html'
                         }).
+                        when('/registrarUsuario', {
+                            controller: 'registrarUsuarioController',
+                            templateUrl: 'app/template/RegistrarUsuario.html'
+                        }).
                         when('/ControlAbejaReina', {
                             controller: 'controlAbejaReinaController',
                             templateUrl: 'app/template/ControlAbejaReina.html'
+                        }).
+                        when('/registrarMarcos', {
+                            controller: 'registrarMarcosController',
+                            templateUrl: 'app/template/RegistrarMarcos.html'
                         }).
                         when('/ControlGastos', {
                             controller: 'controlGastosController',
                             templateUrl: 'app/template/ControlGastos.html'
                         }).
+                        when('/registrarInventario', {
+                            controller: 'registrarInventarioController',
+                            templateUrl: 'app/template/RegistrarInventario.html'
+                        }).
                         when('/ControlInventario', {
                             controller: 'controlInventarioController',
                             templateUrl: 'app/template/ControlInventario.html'
+                        }).
+                        when('/registrarColmenas', {
+                            controller: 'registrarColmenasController',
+                            templateUrl: 'app/template/RegistrarColmenas.html'
                         }).
                         when('/ControlProduccionMiel', {
                             controller: 'controlProduccionMielController',
@@ -29,6 +45,14 @@ angular.module('CAAC').
                         when('/MenuControl', {
                             controller: 'menuControlController',
                             templateUrl: 'app/template/MenuControl.html'
+                        }).
+                        when('/registrarCamaras', {
+                            controller: 'registrarCamarasController',
+                            templateUrl: 'app/template/RegistrarCamaras.html'
+                        }).
+                        when('/registrarApiarios', {
+                            controller: 'registrarApiariosController',
+                            templateUrl: 'app/template/RegistrarApiarios.html'
                         }).
                         otherwise('/');
             }
